86a34832bbcd538c6647ef9b227702591af891f8
jaggu <gondhijagapathi@gmail.com>
2017-05-14 11:10:39 +0530

    final release commit

--- app/src/main/java/jaggu/deviceunique/MainActivity.java
+++ app/src/main/java/jaggu/deviceunique/MainActivity.java
@@ -1,15 +1,76 @@
 package jaggu.deviceunique;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import jaggu.deviceunique.deviceunique.DeviceUnique;
 
 public class MainActivity extends AppCompatActivity {
 
+    int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        checkPermission();
+    }
+    private void Setview(){
+        DeviceUnique ids=new DeviceUnique(this);
+        String text="Android id= "+ids.DeviceId()+"\nImei= "+ids.DeviceIMEI()+"";
+        TextView t=(TextView)findViewById(R.id.test);
+        t.setText(text);
+    }
+    private void checkPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+
+                // MY_PERMISSIONS_REQUEST_READ_PHONE_STATE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+
+        }else{
+            Setview();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 2: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Setview();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
