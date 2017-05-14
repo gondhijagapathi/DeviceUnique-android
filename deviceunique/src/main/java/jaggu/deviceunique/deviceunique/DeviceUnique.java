8546f36d0ae23a7ea17f5c58845d6800bb7ee6de
jaggu <gondhijagapathi@gmail.com>
2017-05-14 09:40:30 +0530

    second commit

--- deviceunique/src/main/java/jaggu/deviceunique/deviceunique/DeviceUnique.java
+++ deviceunique/src/main/java/jaggu/deviceunique/deviceunique/DeviceUnique.java
@@ -7,4 +7,26 @@ package jaggu.deviceunique.deviceunique;
 
 public class DeviceUnique {
 
+    public static String DeviceId(){
+        return null;
+    }
+    public static String DeviceIMEI(){
+        return null;
+    }
+    public static String Device_SIM_ID(){
+        return null;
+    }
+    public static String Device_SIM_serial(){
+        return null;
+    }
+    public static String Device_WIFI_MAC(){
+        return null;
+    }
+    public static String Device_Bluetooth_MAC(){
+        return null;
+    }
+    public static String Device_HardWare_Serial(){
+        return null;
+    }
+
 }
